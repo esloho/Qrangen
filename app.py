1350ad75fea82dd80151282deb160d40d2bc83af
ACR0S <alexcastroros@hotmail.com>
2019-05-25 16:41:55 +0200

    arg parsing added

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+import argparse
 from src.generator import Generator
-import sys
 
 
-def main(mode=0):
+def main():
     qrangen = Generator(mode)
     print(qrangen.generate_number())
 
 
 if __name__ == '__main__':
-    """ 0=local, 1=simulate, 2=ibm"""
-    mode = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', help='Simulation mode: 0 for local | 1 for IBM server simulation | 2 for IBM server REAL experiment', default=0, type=int)
+    args = parser.parse_args()
+    mode = args.mode
 
     if mode not in range(2):
         print(f'Mode {mode} not allowed. Use 0 (local), 1 (remote simulation), or 2 (remote execution)')
