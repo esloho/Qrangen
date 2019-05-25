42c3c3b46d862eb5c7f21a46b765b3979ee869a1
Esther Lozano <esther@seedtag.com>
2019-05-25 18:17:17 +0200

    Use help function in app

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ def main(mode):
     print(qrangen.generate_number())
 
 
+def get_help():
+    return """Simulation mode: 0 for local | 1 for IBM server
+                simulation | 2 for IBM server REAL experiment"""
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-m',
         '--mode',
-        help="""Simulation mode: 0 for local | 1 for IBM server
-                simulation | 2 for IBM server REAL experiment""",
+        help=get_help(),
         default=0,
         type=int)
     args = parser.parse_args()
     mode = args.mode
 
     if mode not in range(2):
-        print(f'Mode {mode} not allowed. Use 0 (local), 1 (remote simulation), or 2 (remote execution)')
+        print(f'Mode {mode} not allowed. {get_help()}')
     else:
         main(mode)
