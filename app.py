92773b99b24b379593d96df58b024b57b17faeb2
Esther Lozano <esther@seedtag.com>
2019-05-25 18:31:51 +0200

    Update help

--- app.py
+++ app.py
@@ -6,22 +6,27 @@ def is_power2(num):
     return num != 0 and ((num & (num - 1)) == 0)
 
 
-def main(mode, upper_bound):
+def main(args):
+    mode = args.mode
+    exponent = args.exponent
+
     if mode not in range(2):
-        print(f'Mode {mode} not allowed. {get_help()}')
+        print(f'Mode {mode} not allowed.')
+        print_help()
         return
 
-    if not is_power2(upper_bound):
-        print(f'Upper bound {upper_bound} not allowed. Needs to be a power of 2!!')
+    if not is_power2(exponent):
+        print(f'Exponent {exponent} not allowed.')
         return
 
     qrangen = Generator(mode)
     print(qrangen.generate_number())
 
 
-def get_help():
-    return """Simulation mode: 0 for local | 1 for IBM server
-                simulation | 2 for IBM server REAL experiment"""
+def print_help():
+    print("""-m --mode : Simulation mode: 0 for local | 1 for IBM server
+                simulation | 2 for IBM server REAL experiment\n\n"""
+          + """"-e --exponent : Generates a random number between 0 and 2**u-1. Needs to be a power of 2""")
 
 
 if __name__ == '__main__':
@@ -29,18 +34,17 @@ if __name__ == '__main__':
     parser.add_argument(
         '-m',
         '--mode',
-        help=get_help(),
+        help="""Simulation mode: 0 for local | 1 for IBM server
+                simulation | 2 for IBM server REAL experiment""",
         default=0,
         type=int)
     parser.add_argument(
-        '-r',
-        '--upper_bound',
-        help="Generates a random number between 0 and 2**u-1. Needs to be a power of 2",
+        '-e',
+        '--exponent',
+        help="""Generates a random number between 0 and 2**u-1. Needs to be a power of 2""",
         default=1,
         type=int)
 
-    args = parser.parse_args()
-    mode = args.mode
-    upper_bound = args.upper_bound
+    parsed_args = parser.parse_args()
 
-    main(mode, upper_bound)
+    main(parsed_args)
--- src/interpreter.py
+++ src/interpreter.py
@@ -1,9 +1,11 @@
 class Interpreter:
+
     def __init__(self):
         pass
 
     def extract_random_number(self, result):
         return [binary2decimal(i) for i in result.get_memory()]
+
 
 def binary2decimal(string):
     maxp = len(string)
