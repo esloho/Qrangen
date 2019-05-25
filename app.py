ff2b70b24ca254e844494fcda1bd1daebe02d00e
Esther Lozano <esther@seedtag.com>
2019-05-25 13:46:48 +0200

    Add main

---
+++ app.py
@@ -0,0 +1,10 @@
+from src.generator import Generator
+
+
+def main():
+    qrangen = Generator()
+    return qrangen.generate_number()
+
+
+if __name__ == '__main__':
+    main()
--- src/models/circuit.py
+++ src/models/circuit.py
@@ -2,15 +2,16 @@ import qiskit
 
 Circuit = H
 
+
 class AnyCircuit:
-    def __init__(self, nqubits = None, nbits = None):
-        if nqubits == None:
+    def __init__(self, nqubits=None, nbits=None):
+        if nqubits is None:
             self.qr = None
             self.cr = None
             self.U = qiskit.QuantumCircuit()
         else:
             self.qr = qiskit.QuantumRegister(nqubits)
-            if nbits == None:
+            if nbits is None:
                 nbits = nqubits
             self.cr = qiskit.ClassicalRegister(nbits)
             self.U = qiskit.QuantumCircuit(self.qr, self.cr)
@@ -32,25 +33,30 @@ class AnyCircuit:
         result.U = circ.U + self.U
         return result
 
-def h(iqubit = 0, nqubits = 1):
+
+def h(iqubit=0, nqubits=1):
     result = AnyCircuit(nqubits)
     result.U.h(result.qr[iqubit])
     return result
 
-def x(iqubit = 0, nqubits = 1):
+
+def x(iqubit=0, nqubits=1):
     result = AnyCircuit(nqubits)
     result.U.x(result.qr[iqubit])
     return result
 
-def y(iqubit = 0, nqubits = 1):
+
+def y(iqubit=0, nqubits=1):
     result = AnyCircuit(nqubits)
     result.U.y(result.qr[iqubit])
     return result
 
-def z(iqubit = 0, nqubits = 1):
+
+def z(iqubit=0, nqubits=1):
     result = AnyCircuit(nqubits)
     result.U.z(result.qr[iqubit])
     return result
 
-def measure(iqubit = 0, nqubits = 1):
+
+def measure(iqubit=0, nqubits=1):
     pass
