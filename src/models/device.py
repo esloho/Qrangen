84f91660771283b3fb957b1fad2543c2abed8009
Esther Lozano <esther@seedtag.com>
2019-05-25 19:09:26 +0200

    Use input values for nqubits and shots

--- src/models/device.py
+++ src/models/device.py
@@ -16,7 +16,8 @@ class IBMDevice:
 class SimulationDevice:
 
     def __init__(self, nqubits):
-        self.backend = least_busy(IBMQ.backends(filters=lambda x: x.configuration().n_qubits >= nqubits and x.configuration().simulator))
+        load_credentials()
+        self.backend = IBMQ.backends(filters=lambda x: x.configuration().n_qubits >= nqubits and x.configuration().simulator)[0]
 
     def execute(self, circuit, shots=1):
         return qiskit_execution(circuit, self.backend, shots)
@@ -35,10 +36,11 @@ def qiskit_execution(circuit, backend, shots):
     q_circuit = circuit.to_qiskit()
     return execute(q_circuit, backend, shots=shots, memory=True)
 
+
 def load_credentials():
     try:
         with open('key.json', 'r+') as token_file:
-            ibm_token_json = json.loads(token_file)
+            ibm_token_json = json.load(token_file)
         ibm_token = ibm_token_json['api_token']
         IBMQ.enable_account(ibm_token)
     except Exception as e:
