ac50d9b7b5e426beca6ec1e8b45eca6efb4c7ed1
Esther Lozano <esther@seedtag.com>
2019-05-25 14:54:58 +0200

    Convert circuit to qiskit before execute it

--- src/models/device.py
+++ src/models/device.py
@@ -4,19 +4,25 @@ from qiskit.providers.ibmq import least_busy
 
 class IBMDevice:
 
-    def __init__(self, nqubits):
+    def __init__(self, nqubits, shots=1024):
         IBMQ.load_accounts()
         self.backend = least_busy(IBMQ.backends(filters=lambda x: x.configuration().n_qubits >= nqubits and not x.configuration().simulator))
+        self.shots = shots
 
     def execute(self, circuit):
-        return self.backend.execute(circuit)
+        return qiskit_execution(circuit, self.backend, self.shots)
 
 
 class LocalDevice:
 
-    def __init__(self, shots):
+    def __init__(self, shots=1024):
         self.backend = Aer.get_backend('qasm_simulator')
         self.shots = shots
 
     def execute(self, circuit):
-        return execute(circuit, self.backend, self.shots)
+        return qiskit_execution(circuit, self.backend, self.shots)
+
+
+def qiskit_execution(circuit, backend, shots):
+    q_circuit = circuit.to_qiskit()
+    return execute(q_circuit, backend, shots=shots)
