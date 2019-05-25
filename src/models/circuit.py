9e78896831e771eccb28ff0fad1558f1445bbbc0
Esther Lozano <esther@seedtag.com>
2019-05-25 16:28:52 +0200

    Fix format

--- src/models/circuit.py
+++ src/models/circuit.py
@@ -1,10 +1,13 @@
 import qiskit
 
+
 class Circuit:
+
     def __init__(self):
         self.qr = qiskit.QuantumRegister(1)
         self.cr = qiskit.ClassicalRegister(1)
         self.U = qiskit.QuantumCircuit(self.qr, self.cr)
         self.U.h(self.qr)
-    def to_qiskit()
+
+    def to_qiskit(self):
         return self.U
